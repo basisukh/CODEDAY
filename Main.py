28eaf89d42d4b5bc69cef6eab1cc4250e50a632b
basisukh <55670819+basisukh@users.noreply.github.com>
2020-03-20 08:52:00 -0700

    Add files via upload
    User Interface

---
+++ Main.py
@@ -0,0 +1,21 @@
+import * [filename]
+import * [filename]
+botname = ' '
+print("Hello, and Welcome to the",botname)
+print("Here are the functions that our", botname, "can do")
+print("A.", [insert function 1])
+print("B.", [insert function 2])
+print("C.", [insert function 3])
+print("D.", [insert function 4])
+print("E.", [insert function 5])
+Chosen_Func = input("What function do you want to do? ")
+if Chosen_Func == a:
+    'go to a'
+if Chosen_Func == b:
+    'go to b'
+if Chosen_Func == c:
+    'go to c'    
+if Chosen_Func == d:
+    'go to d'
+if Chosen_Func == e:
+    'go to e'
