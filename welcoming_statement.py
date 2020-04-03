52e2a8ea1f198a6c04e54be68e10be9ac0947bae
basisukh <55670819+basisukh@users.noreply.github.com>
2020-04-03 12:02:59 -0700

    Delete welcoming_statement.py

--- welcoming_statement.py
+++
@@ -1,2 +0,0 @@
-def greeting():
-    print ("Hi, and welcome to MalBot. This app is strictly focused on making sure you’re getting the nutrients you need as well as the exercise you need to be healthy. We thank you for your cooperation while using our app and sorry for any errors. At the end of the day, we are just a group of 5 middle schoolers.")
